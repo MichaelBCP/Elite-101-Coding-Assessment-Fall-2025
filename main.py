130e3670202168a825ff9233124b2ce0ca67a523
MichaelBCP <M.Ghobrial28@bcp.org>
2025-11-13 03:03:33 -0800

    finished minimum

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from library_books import library_books
 from datetime import datetime, timedelta
+import tkinter as tk
+
+#Tkinter tutorial used: https://www.geeksforgeeks.org/python/python-gui-tkinter
 
 # -------- Level 1 --------
 # TODO: Create a function to view all books that are currently available
@@ -16,9 +19,11 @@ def view_books(library_books):
 # Return a list of matching books
 
 def search_books(library_books, search_term, search_type):
+    #Search terms and info are all changed to lower case to not be case-sensitive
     matches = []
     search_term = search_term.lower()
 
+    #Loops through database, checks for matches
     for book in library_books:
         if search_type == "author":
             if book["author"].lower() == search_term:
@@ -39,6 +44,7 @@ def search_books(library_books, search_term, search_type):
 #   - Print a message saying it's already checked out
 
 def checkout_book(library_books, ID):
+    #Loops through database to find book, uses datetime
     for book in library_books:
         if book["id"] == ID:
             if book["available"]:
@@ -75,6 +81,40 @@ def return_overdue_books(library_books):
 # -------- Level 5 --------
 # TODO: Convert your data into a Book class with methods like checkout() and return_book()
 # TODO: Add a simple menu that allows the user to choose different options like view, search, checkout, return, etc.
+class Book:
+    def __init__(self, id, title, author, genre, available, due_date, checkouts):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.available = available
+        self.due_date = due_date
+        self.checkouts = checkouts
+
+    def checkout(self):
+        if self.available:
+            self.available = False
+            self.due_date = datetime.now() + timedelta(weeks=2)
+            self.checkouts += 1
+        else:
+            print("Sorry, " + self.title + " is not available.")
+
+    def return_book(self):
+        self.available = True
+        self.due_date = None
+
+    def view_details(self):
+        print(self.id+"."+self.title+"."+self.author)
+
+def create_book_objects(library_books):
+    book_list = []
+
+    for book in library_books:
+        #Dictionary Unpacker argument
+        book = Book(**book)
+        book_list.append(book)
+
+    return book_list
 
 # -------- Optional Advanced Features --------
 # You can implement these to move into Tier 4:
@@ -86,5 +126,9 @@ def return_overdue_books(library_books):
 
 if __name__ == "__main__":
     # You can use this space to test your functions
-    print(search_books(library_books, "Historical", "genre"))
-    pass
+    window = tk.Tk()
+
+
+
+    while running:
+        print("")
